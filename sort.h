4e37220f853785ef2bea5d969841824bebf6a061
Tshegofatso <tshegofatsosekhabi@gmail.com>
2023-09-26 02:55:16 -0700

    insert

--- sort.h
+++ sort.h
@@ -43,6 +43,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
